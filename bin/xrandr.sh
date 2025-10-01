1572671a520dd40bb3b894a19bc4574a3caf5149
Alpyg <alpyg@pm.me>
2025-10-01 14:18:52 -0400

    misc: displays

--- bin/xrandr.sh
+++ bin/xrandr.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 xsetroot -cursor_name left_ptr &
-xrandr --output DP-2 --mode 2560x1440 --rate 180 --pos 0x0 --primary \
-       --output DP-1 --mode 1920x1080 --rate 60  --pos 2560x360 &
+xrandr --output DP-0 --mode 2560x1440 --rate 180 --pos 1920x0 --primary \
+       --output DP-3 --mode 1920x1080 --rate 60  --pos 0x180 &
