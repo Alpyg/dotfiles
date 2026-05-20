dcb28b1adb3795bf44abd2f8728395c16f5c7b66
Alpyg <alpyg@pm.me>
2026-05-19 21:57:15 -0400

    add oxwm config

---
+++ pkgs/oxwm-git/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+install() {
+	echo "[${pkg}] Installing config"
+	ln -sf $pkg_dir/oxwm $home/.config
+}
+
+uninstall() {
+	echo "[${pkg}] Uninstalling config"
+	rm -rf $home/.config/oxwm
+}
